d81ef76a71d7206a3e62228299e1caa0e1288711
jmarques <jmarques@energia.org.br>
2020-02-11 13:06:38 -0300

    feat[BE]: Import project

---
+++ backend/crud-backend/bin/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Pessoas
+
+INSERT INTO CRUD_PESSOA (id_pessoa, nome_pessoa) VALUES (SEQ_CRUD_PESSOA.nextval, 'Jose Eduardo Trindade E Marques');
+INSERT INTO CRUD_PESSOA (id_pessoa, nome_pessoa) VALUES (SEQ_CRUD_PESSOA.nextval, 'Sarah De Villio');
+INSERT INTO CRUD_PESSOA (id_pessoa, nome_pessoa) VALUES (SEQ_CRUD_PESSOA.nextval, 'Marcelo Molina');
+
+-- Documentos
+
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'CPF', 'CPF', '99988877766', 1);
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'RG', 'RG', '998887776', 1);
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'CPF', 'CPF', '11122233344', 2);
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'RG', 'RG', '112223334', 2);
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'CPF', 'CPF', '98765432199', 3);
+--INSERT INTO CRUD_DOCUMENTO (id_documento, codigo_documento, descricao_documento, valor_documento, id_pessoa) VALUES (SEQ_CRUD_DOCUMENTO.nextval, 'RG', 'RG', '987654321', 3);
