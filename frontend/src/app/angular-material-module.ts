7bf35e2241a7fcdcd34e2500a0067fbff9000726
jmarques <jmarques@energia.org.br>
2020-03-03 15:10:33 -0300

    feat[FE]: Add Angular Material Module

---
+++ frontend/src/app/angular-material-module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+   MatButtonModule,
+   MatToolbarModule,
+
+} from '@angular/material';
+
+@NgModule({
+   imports: [
+      CommonModule,
+      MatButtonModule,
+      MatToolbarModule,
+   ],
+   exports: [
+      MatButtonModule,
+      MatToolbarModule,
+   ],
+   providers: []
+})
+
+export class AngularMaterialModule { }
