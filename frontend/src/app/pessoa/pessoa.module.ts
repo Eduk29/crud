a11ba0ed2ad36fb25e15270e405f93a77e1d6030
jmarques <jmarques@energia.org.br>
2020-03-05 19:18:06 -0300

    (WIP) feat[FE]: Add Pessoa List

--- frontend/src/app/pessoa/pessoa.module.ts
+++ frontend/src/app/pessoa/pessoa.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PessoaComponent } from './pessoa.component';
 import { PessoaListComponent } from './pessoa-list/pessoa-list.component';
+import {MatPaginatorModule} from '@angular/material';
+import {MatTableModule} from '@angular/material';
 
 @NgModule({
   declarations: [PessoaComponent, PessoaListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule, MatTableModule],
   exports: [PessoaListComponent]
 })
 export class PessoaModule {}
