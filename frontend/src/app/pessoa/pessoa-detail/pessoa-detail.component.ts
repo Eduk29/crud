e8a25fd0dd10f950236fea0469a45820a8a4a7d6
jmarques <jmarques@energia.org.br>
2020-03-06 17:52:18 -0300

    feat[FE]: Add pessoa-detail

---
+++ frontend/src/app/pessoa/pessoa-detail/pessoa-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-detail',
+  templateUrl: './pessoa-detail.component.html',
+  styleUrls: ['./pessoa-detail.component.scss']
+})
+export class PessoaDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
