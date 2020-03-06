b16497b90d8b32550c5cc9bf1c8ea48f05ab3f08
jmarques <jmarques@energia.org.br>
2020-03-06 16:31:01 -0300

    refactor[FE]: Remove Pessoa service call from Home Component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PessoaService } from 'src/app/services/pessoa.service';
-import { Pessoa } from 'src/app/models/Pessoa.model';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +7,7 @@ import { Pessoa } from 'src/app/models/Pessoa.model';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private pessoaService: PessoaService) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.getPessoa();
-  }
-
-  getPessoa(): void {
-    this.pessoaService
-      .listPessoas()
-      .subscribe((response: Array<Pessoa>) => {
-        console.log('Response: ', response);
-      });
-  }
-
+  ngOnInit() { }
 }
