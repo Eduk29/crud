907fcb4628bc6aa85cf07560fc847e60d0dbceb1
Eduk29 <edu.temarques@gmail.com>
2020-03-08 18:26:20 -0300

    refactor[FE]: Adjust in Lazy Load Module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PessoaModule } from './pessoa/pessoa.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'pessoa',
+    loadChildren: () => PessoaModule,
   }
 ];
 
